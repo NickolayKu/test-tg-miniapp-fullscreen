2435fed306991df3224031ebd13523494d82d625
NickolayKu <nickolay135000@yandex.ru>
2025-08-07 19:33:18 +0300

    Changed function app

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,36 @@
 import { miniApp, viewport } from '@telegram-apps/sdk';
+import { useEffect } from 'react';
 
-async function App() {
-  
-  if (miniApp.mountSync.isAvailable()) {
-    miniApp.mountSync();
-    miniApp.isMounted(); // true
-  }
-
-  if (viewport.mount.isAvailable()) {
-    try {
-      const promise = viewport.mount();
-      viewport.isMounting(); // true
-      await promise;
-      viewport.isMounting(); // false
-      viewport.isMounted(); // true
-    } catch (err) {
-      viewport.mountError(); // equals "err"
-      viewport.isMounting(); // false
-      viewport.isMounted(); // false
+function App() {
+    const telegram = async () => {
+      if (miniApp.mountSync.isAvailable()) {
+        miniApp.mountSync();
+        miniApp.isMounted(); // true
+      }
+    
+      if (viewport.mount.isAvailable()) {
+        try {
+          const promise = viewport.mount();
+          viewport.isMounting(); // true
+          await promise;
+          viewport.isMounting(); // false
+          viewport.isMounted(); // true
+        } catch (err) {
+          viewport.mountError(); // equals "err"
+          viewport.isMounting(); // false
+          viewport.isMounted(); // false
+        }
+      }
+    
+      if (viewport.requestFullscreen.isAvailable()) {
+        await viewport.requestFullscreen();
+        viewport.isFullscreen(); // true
+      }
     }
-  }
 
-  if (viewport.requestFullscreen.isAvailable()) {
-    await viewport.requestFullscreen();
-    viewport.isFullscreen(); // true
-  }
+    useEffect(() => {
+      telegram();
+    }, [])
 
   return (
     <>
